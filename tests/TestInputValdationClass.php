ee5c208e48f33a45a62ca483a806ad5c32f9254e
Roland <rolandgolla@gmail.com>
2022-09-18 23:22:56 +0200

    Use setup and add php 8.1
    - add "php": "8.1" to composer json
    - add setup method to rtest for class

--- tests/TestInputValdationClass.php
+++ tests/TestInputValdationClass.php
@@ -2,11 +2,17 @@
 
 class TestInputValdationClass extends \PHPUnit\Framework\TestCase
 {
+    private \App\InputValidationClass $inputValidationClass;
+
+    public function setUp(): void
+    {
+        $inputValidationClass = new \App\InputValidationClass();
+    }
+
     /**
      * @testdox Let the return true method run without any mocking
      */
     public function testReturnTrueReturnTrue() {
-        $inputValidationClass = new \App\InputValidationClass();
-        $this->assertTrue($inputValidationClass->returnTrue(), 'The method only can return true');
+        $this->assertTrue($this->inputValidationClass->returnTrue(), 'The method only can return true');
     }
 }
