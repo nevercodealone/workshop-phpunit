c9c66f4d428d6c09d454e8f8b93c06e3436e8806
Roland <rolandgolla@gmail.com>
2022-09-18 23:12:36 +0200

    Better outputb for tests
    - Assert true message and let test fail by setting return to false
    - Add testdox annotation for better test reading

--- tests/TestInputValdationClass.php
+++ tests/TestInputValdationClass.php
@@ -2,8 +2,11 @@
 
 class TestInputValdationClass extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @testdox Let the return true method run without any mocking
+     */
     public function testReturnTrueReturnTrue() {
         $inputValidationClass = new \App\InputValidationClass();
-        $this->assertTrue($inputValidationClass->returnTrue());
+        $this->assertTrue($inputValidationClass->returnTrue(), 'The method only can return true');
     }
 }
