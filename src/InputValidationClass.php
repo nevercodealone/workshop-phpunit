a61b167fe48e63165d4e0bdaee1f7ca1f861bc5b
Roland <roland@nevercodealone.de>
2022-09-19 14:05:17 +0200

    Add external service
    - Add ExtremServiceClass
    - Use new service in InputValidationClass
    - Add test with mock

--- src/InputValidationClass.php
+++ src/InputValidationClass.php
@@ -3,8 +3,16 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\ExternalSpamDetection;
+
 final class InputValidationClass
 {
+
+
+    public function __construct(private ExternalSpamDetection $externalSpamDetection)
+    {
+    }
+
     public function returnTrue(): bool
     {
         return true;
@@ -13,6 +21,10 @@ final class InputValidationClass
     public function isValidEmail(string $email): bool
     {
         if($this->filterEmail($email) === false) {
+            return false;
+        }
+
+        if($this->externalSpamDetection->isSpam($email) === false) {
             return false;
         }
 
